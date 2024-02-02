1d1f532a0569009a86de64aa9187e0d5b34f7d06
Sumit Singh <sumitsingh310106@gmail.com>
2024-02-02 23:16:19 +0530

    Completed the game

--- The Simon Say Game/app.js
+++ The Simon Say Game/app.js
@@ -5,6 +5,7 @@ let btns = ["yellow", "red","purple","green"];
 
 let started = false;
 let level = 0;
+// let hScore = 0;
 
 let h2 = document.querySelector("h2");
 
@@ -31,7 +32,9 @@ function userFlash(btn) {
     }, 300);
 }
 
+
 function levelUp(){
+    userSeq = [];
     level++;
     h2.innerText=`Level ${level}`;
 
@@ -40,19 +43,58 @@ function levelUp(){
     let randIdx = Math.floor(Math.random()*3);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-    console.log(randColor);
-    console.log(randIdx);
-    console.log(randBtn);
+    // console.log(randColor);
+    // console.log(randIdx);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
     gameFlash(randBtn);
+}
+
+function checkAns(idx){
+    // console.log("curr level : ", level);
+    // let idx = level-1;
+ 
+    if(userSeq[idx] == gameSeq[idx]){
+        // console.log("same value"); 
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        } 
+    } else{
+        h2.innerHTML=`Game Over! <br> Your score was <b> ${level}</b> <br> Press any keyboard key to start.`;
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="white";
+        }, 150);
+        reset();
+    }
 }
 
 function btnPress() {
     console.log(this);
     let btn = this;
     userFlash(btn);
+
+    userColor= btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for (btn of allBtns) {
     btn.addEventListener("click",btnPress);
 } 
+
+function reset() {
+    
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
+
+// if(hScore<level){
+//     hScore = level;
+// }
